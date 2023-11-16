a3f84f2157e761019d3af71818d35abcf0e278bb
unknown <anastasiatevzaia@gmail.com>
2023-11-16 23:59:54 +0400

    Add header.php

--- app/public/wp-content/themes/gpjtheme/header.php
+++ app/public/wp-content/themes/gpjtheme/header.php
@@ -16,6 +16,9 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500;700&family=Montserrat:ital,wght@0,600;0,700;1,600;1,700&family=Open+Sans:wght@400;600;700;800&display=swap" rel="stylesheet">
 
 	<?php wp_head(); ?>
 </head>
@@ -25,35 +28,37 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'gpjtheme' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$gpjtheme_description = get_bloginfo( 'description', 'display' );
-			if ( $gpjtheme_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $gpjtheme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+	<header id="masthead" class="site-header gpj-header>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'gpjtheme' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
+				<div class="container">
+					<div class="header-content">
+						<div class="site-branding">
+					<?php
+					the_custom_logo();
+					?>
+				</div><!-- .site-branding -->
+
+				<nav id="site-navigation" class="main-navigation gpj-navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'gpjtheme' ); ?></button>
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+							'menu_class' => 'header-menu',
+						)
+					);
+					?>
+				</nav><!-- #site-navigation -->
+                  <div class="img">
+						<img src="<?php echo get_template_directory_uri() . '/assets/images/star.svg'; ?>" alt="">
+						<img src="<?php echo get_template_directory_uri() . '/assets/images/pf.svg'; ?>" alt="">
+				  </div>
+				 
+			</div>
+			
+
+		</div>
+		
+	
 	</header><!-- #masthead -->
